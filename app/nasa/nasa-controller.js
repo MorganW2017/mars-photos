9e51e88890e60f4dff14f248db20aa527f563639
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2017-11-08 16:05:35 -0700

    Adds filter

--- app/nasa/nasa-controller.js
+++ app/nasa/nasa-controller.js
@@ -7,18 +7,47 @@ function NasaController(){
     }
     //PRIVATE
     function draw(photoArr){
-        console.log('running?')
-        console.log(photoArr)
+        var galleryElem = document.getElementById('gallery')
+        var template = ''
+        for (var i = 0; i < photoArr.length; i++) {
+            var photo = photoArr[i];
+            template += `
+            <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="${photo.img_src}">
+                <div class="caption">
+                    <h3>${photo.camera.full_name}</h3>
+                    <p>${photo.earth_date}</p>
+                    <p>${photo.rover.name}</p>
+                </div>
+            </div>
+        </div>
+        `
+        }
+        galleryElem.innerHTML = template
     }
+    
+    
+    
+    //PUBLIC
+    this.newSol = function newSol(e){
+        debugger
+
+        e.preventDefault()
+        var sol = e.target.sol.value
+        nasaService.getPhotos(sol, draw)
+    }
+    
+
+    this.camFilter = function camFilter(e){
+        e.preventDefault()
+        var camName = e.target.camera.value
+        var filtered = nasaService.camFilter(camName)
+        draw(filtered)
+    }
+    
+    
+    
     nasaService.getPhotos(100,draw)
     console.log('6')
-
-
-
-    //PUBLIC
-
-
-
-
-
 }
--- app/nasa/nasa-service.js
+++ app/nasa/nasa-service.js
@@ -2,22 +2,24 @@ function NasaService(ready) {
 
     console.log("2")
 
-    var sol = 1000
-    var baseUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&api_key=`
-    var key = 'BDALzrV2ji60AQxKUMaHvG6s8diwPLVS8Yy7zEKu'
+    var baseUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=`
+    var key = '&api_key=BDALzrV2ji60AQxKUMaHvG6s8diwPLVS8Yy7zEKu'
     var photos = []
 
+    
 
-    //getPhotos
-
-    //newSearch
-
-    //filter by cam
+    this.camFilter = function camFilter(camName){
+        //check camera.name vs camName
+        return photos.filter(function(photo){
+            if(photo.camera.name == camName){
+              return true;
+            }
+          });
+    }
 
     this.getPhotos = function getPhotos(solQuery, cb) {
         console.log('5')
-        sol = solQuery
-        $.get(baseUrl + key)
+        $.get(baseUrl + solQuery + key)
             .then((res) => {
                 console.log("8")
                 photos = res.photos
